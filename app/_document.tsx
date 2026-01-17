40128b35f2298d072ec43625172a38b747e5d166
Matei Iorga <mateiiorga2009@gmail.com>
2026-01-17 22:09:34 +0100

    Recuperado _document.tsx con meta tag de Google

--- app/_document.tsx
+++ app/_document.tsx
@@ -1,1 +1,16 @@
+import { Html, Head, Main, NextScript } from "next/document";
 
+export default function Document() {
+  return (
+    <Html lang="es">
+      <Head>
+        {/* Meta tag de verificación de Google */}
+        <meta name="google-site-verification" content="8Q_S8I2dt_IDb1Z40YjwGCuEjHqR0w1zBOS4hq4pGFA" />
+      </Head>
+      <body>
+        <Main />
+        <NextScript />
+      </body>
+    </Html>
+  );
+}
